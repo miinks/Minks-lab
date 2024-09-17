db4add487894528cbda0505f9366eb2ff474d9c2
Regie Agustin <124678791+miinks@users.noreply.github.com>
2024-09-17 07:14:11 -0500

    navbar

---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+const Navbar  = () => {
+    return (   
+        <nav className="navbar">
+            <h1>Minks Lab</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Lab</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar  
