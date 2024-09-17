6f4e6efd7e3e2b58db55c11aece33116abd0586a
Regie Agustin <124678791+miinks@users.noreply.github.com>
2024-09-17 07:04:54 -0500

    titles and likes constants

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new Lab";
+  const likes = 50;
   return (
     <div className="App">
       <div className="content">
-       <h1>App Component
+       <h1>{title}
        </h1>
       </div>
     </div>
@@ -12,3 +14,4 @@ function App() {
 }
 
 export default App;
+
