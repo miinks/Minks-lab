3b9afcb227e407a3d09101812970335a499935cf
Regie Agustin <124678791+miinks@users.noreply.github.com>
2024-09-17 07:10:06 -0500

    deleted test data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 
 function App() {
-  const title = "Welcome to the new Lab";
-  const likes = 50;
   return (
     <div className="App">
       <div className="content">
-       <h1>{title}
-       </h1>
       </div>
+
+
     </div>
   );
 }
