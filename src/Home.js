168a2494abb9916e7858154855e62822616d94b9
Regie Agustin <124678791+miinks@users.noreply.github.com>
2024-09-18 02:36:39 -0500

    button onclick argument for click me again

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="home">
            <h2>Homepage</h2> 
            <button onClick={handleClick}>Click Me</button>
-           <button>Click me again</button>
+           <button onClick={}>Click Me Again</button>
         </div>
       );
 }
