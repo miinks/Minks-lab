bad0faa41c76a58bcaa2d0ab563fe9e30298cdc8
Regie Agustin <124678791+miinks@users.noreply.github.com>
2024-09-21 02:53:21 -0500

    new title

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
 
 
   const [name, setName] = useState('mario');
