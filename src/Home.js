f0372b5ff633496c7fa6f976f5dfa271c325d5f3
Regie Agustin <124678791+miinks@users.noreply.github.com>
2024-09-21 02:55:19 -0500

    deleted dupe

--- src/Home.js
+++ src/Home.js
@@ -6,16 +6,7 @@ const Home = () => {
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-
-
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
-
-
-  const handleClick = (e) => {
-    setName('luigi');
-    setAge(30);
-  }
+  })
 
   return (
     <div className="home">
