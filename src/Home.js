675f7ceeb88a42202e8de81f79713cf8ac852ad2
Regie Agustin <124678791+miinks@users.noreply.github.com>
2024-09-21 02:57:41 -0500

    fixed syntax errors

--- src/Home.js
+++ src/Home.js
@@ -6,15 +6,15 @@ const Home = () => {
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  })
-
+  ])
   return (
     <div className="home">
-      <div className="blog-preview" key={blog.id} >      <p>{ name } is { age } years old</p>
-      <h2>{ blog.title }</h2>
-      <p>Written by { blog.author }</p>
-      </div>
-    ))}
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
   );
 }
