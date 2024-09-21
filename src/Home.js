55dd59917a811944c6541b1c4c937c45fd23023a
Regie Agustin <124678791+miinks@users.noreply.github.com>
2024-09-21 02:54:30 -0500

    closing syntax

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
 
 
   const [name, setName] = useState('mario');
